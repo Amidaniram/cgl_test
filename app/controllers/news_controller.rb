83c7c8cff4dd8a86f2385051a28871fb5cc9fcdd
Amidaniram <amidaniram@yandex.ru>
2015-02-01 10:07:10 +0300

    News editing and destroying

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -18,8 +18,17 @@ class NewsController < ApplicationController
     end
   end
 
-  def delete
-
+  def destroy
+    @news = News.find(params[:id])
+    if @news.profile == User.find(session[:user_id]).profile
+      if @news.delete
+        redirect_to game_root_path
+      else
+        redirect_to news_path(@news, game: params[:game])
+      end
+    else
+      redirect_to game_root_path
+    end
   end
 
   def index
@@ -27,8 +36,25 @@ class NewsController < ApplicationController
     redirect_to game_root_path
   end
 
-  def update
+  def edit
+    @news = News.find(params[:id])
+    if !session[:user_id] || @news.profile != User.find(session[:user_id]).profile
+      flash[:notice] = "Не твоя новость, не тебе и править!"
+      redirect_to game_root_path
+    end
+  end
 
+  def update
+    @news = News.find(params[:id])
+    if @news.profile == User.find(session[:user_id]).profile
+      if @news.update_attributes(news_params)
+        redirect_to news_path(@news, game: params[:game])
+      else
+        render 'edit'
+      end
+    else
+      redirect_to game_root_path
+    end
   end
 
   def show
